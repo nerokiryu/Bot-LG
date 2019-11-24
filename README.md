5d95406b0d58d96f73b394c3122193c0c718d5de
Unknown <nero.kiryu@gmail.com>
2019-11-24 16:33:39 +0100

    bot v1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Bot-LG
+# Bot-LG A discord bot t emulate the little girl role
+
+## Install
+
+Download the project and customize the files :
+
+- auth.json : with the id of your bot
+- parameters.json : with the id of the gm and the id of the wolf chanel and the id of the chanel for the little girl ( right clck on th chanel/user and copy identifier)
+
+ `optional : customize the dictonaire.txt file for your language (default french)`
+
+## Uses
+
+all your commands are prefixed by '!'
+
+!help to obtain all the commands
---
+++ bot.js
@@ -0,0 +1,128 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var fs = require('fs');
+
+var auth = require('./auth.json');
+var params = require('./parameters.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'info';
+var wolf_chanel=params.Chanel_wolf;
+var little_girl_chanel=params.Chanel_little_girl;
+var chances = 25;
+var insultes = fs.readFileSync('dictionaire.txt').toString().split("\r\n");
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Check if the it's someone in the wolf chanel
+    if (channelID == wolf_chanel) {
+        var random = Math.floor(Math.random() * Math.floor(100))+1
+        if (random < chances){
+            bot.sendMessage({
+                to: little_girl_chanel,
+                message: message
+            });
+        }
+    }
+    // listen for messages that will start with `!`
+    else if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        switch(cmd) {
+            // !help
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: '!chanel_little_girl <chanel_id> : change the little girl chanel (only the GM can do that)\r\n !chanel_wolf <chanel_id> : change the wolf chanel (only the GM can do that)\r\n !chances <number between 0 and 100> : change the probability to eavesdrop (only the GM can do that)\r\n !ping : respond pong!\r\n !insult : insult yourself\r\n '
+                });
+            break;
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // !insult
+            case 'insult':
+                var insult=insultes[Math.floor(Math.random() * Math.floor(insultes.length))].slice(0, -1);
+                bot.sendMessage({
+                    to: channelID,
+                    message: insult+' ! '+"<@!" + userID + ">"
+                });
+            break;
+            // !chances
+            case 'chances':
+                if(params.GM_ID == userID){
+                    var response;
+                    if(typeof args[1] !== 'undefined' && !isNaN(parseInt(args[1], 10)) && args[1] <= 100 && args[1] >= 0){
+                        response = 'Chances of a message being eavesdrop changed to '+args[1]+'%'
+                        chances = parseInt(args[1], 10);
+                    }else{
+                        response = 'You MotherFucker! Wrong argument your message must be in this form : !chances <number between 0 and 100>'
+                    }
+                    bot.sendMessage({
+                        to: channelID,
+                        message: response
+                    });
+                }
+            break;
+            // !chanel_wolf
+            case 'chanel_wolf':
+                if(params.GM_ID == userID){
+                    var response;
+                    if(typeof args[1] !== 'undefined' && !isNaN(parseInt(args[1], 10))){
+                        response = 'The new chanel of the wolfs is '+"<@!" + args[1] + ">"
+                        wolf_chanel = args[1];
+                    }else{
+                        response = 'You MotherFucker! Wrong argument your message must be in this form : !chanel_wolf <chanel_id>'
+                    }
+                    bot.sendMessage({
+                        to: channelID,
+                        message: response
+                    });
+                }
+            break;
+            // !chanel_little_girl
+            case 'chanel_little_girl':
+                if(params.GM_ID == userID){
+                    var response;
+                    if(typeof args[1] !== 'undefined' && !isNaN(parseInt(args[1], 10))){
+                        response = 'The new chanel of the little girl is '+"<@!" + args[1] + ">"
+                        little_girl_chanel = args[1];
+                    }else{
+                        response = 'You MotherFucker! Wrong argument your message must be in this form : !chanel_little_girl <chanel_id>'
+                    }
+                    bot.sendMessage({
+                        to: channelID,
+                        message: response
+                    });
+                }
+            break;
+         }
+     }
+     // this is not a wolf or command
+     else{
+        insultes.forEach(function(element) {
+            if(message.includes(element)){
+                logger.debug('insulte détecté !');
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Hey their is kids here, you mother fucker! '+"<@!" + userID + ">"
+                });
+                
+            }
+        });
+     }
+});
